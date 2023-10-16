55ffb176f6607509fe8efde1ecd2a84ca79adead
ajgrinds <ajgrinds03@gmail.com>
2023-10-15 18:54:02 -0700

    First website

--- getbuzzed/src/App.js
+++ getbuzzed/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import HomePage from './HomePage.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://cdn-icons-png.flaticon.com/128/807/807303.png"></img>
-        <p>
-          Get Buzzed website under construction.
-        </p>
-      </header>
+        <HomePage></HomePage>
     </div>
   );
 }
